8ea1806b9f96c3c0d0f3639d3295ecdfccdddea4
Harrison Smith <harrisonws320@gmail.com>
2023-11-27 08:33:43 -0600

    I neglected saving to github last week.

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,3 +1,9 @@
+//Step 02) To generate the C# models:
+//Scaffold-DbContext "Server=.\sqlexpress;Database=ToDo;Trusted_Connection=true;MultipleActiveResultSets=true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+
+//Step 03) Add the using statement below:
+using Microsoft.EntityFrameworkCore;
+
 namespace ToDoAPI
 {
     public class Program
@@ -7,11 +13,34 @@ namespace ToDoAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //Step 10a) Add CORS functionality to determine what websites can access the data in this application
+            //CORS stands for Cross Origin Resource Sharing and by default browsers use this to block websites from requesting data unless that website has permission to do so. This code below determines what websites have access to CORS with this API.
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.WithOrigins("OriginPolicy", "http://localhost:3000", "http://todo.harrisonwsmith.com").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //Step 04) Add the ResourcesContext Service
+            //This will initialize the database connection to be used in the controllers
+            builder.Services.AddDbContext<ToDoAPI.Models.ToDoContext>(
+                options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoDB"));
+                    //The string above ("ToDoDB") should match the connectionString name in appsettings.json
+                }
+              );
+
+            //Step 05) Scaffold a new API controller using Entity Framework - Scaffold Categories choosing the Categories model,
+            //the ResourcesContext for DataContext. After walk thru the code in the controller and test using the browser (Swagger).
+            //Looking for next step? Open ResourcesController.
 
             var app = builder.Build();
 
@@ -28,6 +57,9 @@ namespace ToDoAPI
 
 
             app.MapControllers();
+
+            //Step 10b) add UseCors statement below:
+            app.UseCors();
 
             app.Run();
         }
